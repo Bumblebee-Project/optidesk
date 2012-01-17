9c5c35cc91fd4180737a49e941042240609f449e
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2012-01-17 15:56:35 -0800

    Corrected the Copyleft statement

--- optidesk.c
+++ optidesk.c
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2011 Bumblebee Project
+ * Copyright (C) 2012 The Bumblebee Project
  * Author: Joaquín Ignacio Aramendía <samsagax@gmail.com>
  *
- * This file is part of Bumblebee.
+ * This file is part of Optidesk.
  *
- * Bumblebee is free software: you can redistribute it and/or modify
+ * Optidesk is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Bumblebee is distributed in the hope that it will be useful,
+ * Optidesk is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Bumblebee. If not, see <http://www.gnu.org/licenses/>.
+ * along with Optidesk. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
