6fddd719b588b1e88ccddf5c4056238df2f875bb
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2012-01-18 03:18:04 -0300

    Added basic argument parsing, help and version messages.

--- optidesk.c
+++ optidesk.c
@@ -18,12 +18,73 @@
  * along with Optidesk. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 #include <gio/gio.h>
 
+/**
+ * Print version string
+ */
+static void print_version(void) {
+    printf("%s version %s\n", "optidesk", "0.1");
+}
+
+/**
+ * Formatted help string
+ */
+static void print_opt_line(char* opt, char* description) {
+    printf(" %-25s%s\n", opt, description);
+}
+
+/**
+ * Print a little note on usage
+ */
+static void print_help(int exit_val) {
+    print_version();
+    printf("Usage: %s [options] <orig> <dest>\n", "optidesk");
+    print_opt_line("-h / --help","Print this help message");
+    print_opt_line("-V / --version","Print version and exit");
+    exit(exit_val);
+}
+
+
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("Usage: optidesk <source> <destination>\n");
+    // Parse the options
+    char* short_opts= "Vh";
+    struct option long_opts[] = {
+        {"version"  , no_argument, 0, 'V'},
+        {"help"     , no_argument, 0, 'h'},
+        {0          , 0, 0, 0}
+    };
+    char c;
+    while(1) {
+        c = getopt_long(argc, argv, short_opts, long_opts, 0);
+        if (c == -1) {
+            break;
+        }
+        switch (c) {
+            case 'V':
+                print_version();
+                break;
+            case 'h':
+                print_help(EXIT_SUCCESS);
+                break;
+            case '?':
+                print_help(EXIT_FAILURE);
+                break;
+            default:
+                printf("?? getopt returned %o ??\n", c);
+        }
+    }
+    if ((argc - optind) < 2) {
+        printf("Not enough arguments\n");
+        printf("expected: 2\ngot: %d\n", argc - optind);
+        exit(EXIT_FAILURE);
+    } else if ((argc - optind) > 2) {
+        printf("Too many arguments\n");
+        printf("expected: 2\ngot: %d\n", argc - optind);
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
